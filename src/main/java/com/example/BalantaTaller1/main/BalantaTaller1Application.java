6cc11688c2a14a49bb2249811384f9747e879e9e
Manuel Balanta <mbalantaq@gmail.com>
2022-03-08 20:08:36 -0500

    App config

---
+++ src/main/java/com/example/BalantaTaller1/main/BalantaTaller1Application.java
@@ -0,0 +1,13 @@
+package com.example.BalantaTaller1.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalantaTaller1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalantaTaller1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BalantaTaller1/BalantaTaller1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BalantaTaller1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalantaTaller1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
