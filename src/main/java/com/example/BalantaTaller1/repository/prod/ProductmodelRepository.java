835ac59b3af10d07f245524059835cff9621837c
Manuel Balanta <mbalantaq@gmail.com>
2022-03-23 00:59:51 -0500

    First test failed

--- src/main/java/com/example/BalantaTaller1/repository/prod/ProductmodelRepository.java
+++ src/main/java/com/example/BalantaTaller1/repository/prod/ProductmodelRepository.java
@@ -1,9 +1,11 @@
 package com.example.BalantaTaller1.repository.prod;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.BalantaTaller1.model.prod.Productmodel;
 
+@Repository
 public interface ProductmodelRepository extends JpaRepository<Productmodel, Integer>{
 
 }
--- src/test/java/com/example/BalantaTaller1/integration/ProductcategoryITest.java
+++ src/test/java/com/example/BalantaTaller1/integration/ProductcategoryITest.java
@@ -4,27 +4,64 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.example.BalantaTaller1.main.BalantaTaller1Application;
+import com.example.BalantaTaller1.model.prod.Productcategory;
+import com.example.BalantaTaller1.repository.prod.ProductcategoryRepository;
+import com.example.BalantaTaller1.service.prod.ProductcategoryService;
+import com.example.BalantaTaller1.service.prod.ProductcategoryServiceImpl;
 
-//@ExtendWith(SpringExtension.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = BalantaTaller1Application.class)
+@ContextConfiguration(classes = ProductcategoryRepository.class)
 class ProductcategoryITest {
 
+	private ProductcategoryRepository productcategoryRepository;
+	private ProductcategoryService productcategoryService;
+
+	@Autowired
+	public ProductcategoryITest(ProductcategoryRepository productcategoryRepository) {
+		this.productcategoryRepository = productcategoryRepository;
+		this.productcategoryService = new ProductcategoryServiceImpl(productcategoryRepository);
+	}
+
 	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
+	static void start() {
+		System.out.println("... TEST STARTED ...");
+	}
+
+	@Nested
+	@DisplayName("Save methods")
+	class ProductcategoryServiceSave {
+		@Test
+		public void saveCorrectly() {
+			Productcategory productCategory = new Productcategory();
+			
+			try {
+				productCategory.setProductcategoryid(13);
+				productCategory.setName("Hogar");
+				productCategory.setModifieddate(null);
+				productCategory.setProductsubcategories(null);
+				productcategoryService.save(productCategory);
+			} catch (RuntimeException e) {
+				e.printStackTrace();
+			}
+			assertTrue(productCategory.equals(productcategoryRepository.findById(productCategory.getProductcategoryid()).get()));
+			//fail("Not yet implemented");
+		}
 	}
 
 	@AfterAll
-	static void tearDownAfterClass() throws Exception {
-	}
-
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	static void finish() {
+		System.out.println("... TEST FINISHED ...");
 	}
 
 }
