06489c21bff394d1a1d2a63db847c45353ee5ec3
Manuel Balanta <mbalantaq@gmail.com>
2022-03-29 20:25:17 -0500

    Integration test running

--- src/main/java/com/example/BalantaTaller1/main/BalantaTaller1Application.java
+++ src/main/java/com/example/BalantaTaller1/main/BalantaTaller1Application.java
@@ -2,10 +2,16 @@ package com.example.BalantaTaller1.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 //import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-//@ComponentScan(basePackages = {"com.example.BalantaTaller1"})
+@ComponentScan(basePackages = {"com.example.BalantaTaller1"})
+@EnableJpaRepositories(basePackages = "com.example.BalantaTaller1")
+@EntityScan(basePackages = "com.example.BalantaTaller1")
 public class BalantaTaller1Application {
 
 	public static void main(String[] args) {
