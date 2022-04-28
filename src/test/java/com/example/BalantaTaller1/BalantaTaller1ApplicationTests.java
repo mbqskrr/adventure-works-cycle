1eba1d1aea5a4f94154137815dcb33d4b5c68095
Manuel Balanta <mbalantaq@gmail.com>
2022-04-28 17:03:31 -0500

    Integration test for Productcategory, done

--- src/test/java/com/example/BalantaTaller1/BalantaTaller1ApplicationTests.java
+++ src/test/java/com/example/BalantaTaller1/BalantaTaller1ApplicationTests.java
@@ -2,7 +2,11 @@ package com.example.BalantaTaller1;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
+import com.example.BalantaTaller1.main.BalantaTaller1Application;
+
+@ContextConfiguration(classes = BalantaTaller1Application.class)
 @SpringBootTest
 class BalantaTaller1ApplicationTests {
 
--- src/test/java/com/example/BalantaTaller1/integration/ProdcutcategoryIntegrationTest.java
+++ src/test/java/com/example/BalantaTaller1/integration/ProdcutcategoryIntegrationTest.java
@@ -1,11 +1,15 @@
 package com.example.BalantaTaller1.integration;
 
+//import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+//import static org.mockito.Mockito.when;
 
 import java.sql.Timestamp;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterAll;
@@ -28,16 +32,16 @@ import com.example.BalantaTaller1.repository.prod.ProductcategoryRepository;
 class ProdcutcategoryIntegrationTest {
 	
 	private Productcategory pc;
+	private Productcategory productcategory;
+	
 	private SimpleDateFormat df; 
 	private Date date; 
 	private long time1;
 	private Timestamp time;
 
 	private ProductcategoryRepository productcategoryRepository;
-	
 	@Autowired
 	public ProdcutcategoryIntegrationTest(ProductcategoryRepository productcategoryRepository) {
-	
 		this.productcategoryRepository = productcategoryRepository;
 	}
 
@@ -54,46 +58,102 @@ class ProdcutcategoryIntegrationTest {
 		date  = df.parse("24-03-2022");
 		time1 = date.getTime();
 		time = new Timestamp(time1);
+		
+		pc.setName("Hogar");
+		pc.setRowguid(14);
+		pc.setModifieddate(time);
+		
+		pc.setProductsubcategories(null);
+		
+		productcategory = productcategoryRepository.save(pc);
 	}
 	
-	@Nested
+	/*@Nested
 	@DisplayName("Save methods")
 	class ProductcategoryServiceSave {
 
 		@Test
 		@Order(1)
-		public void saveCorrectly() {
+		public void save() {
 			
-			pc.setProductcategoryid(7);
+			
 			pc.setName("Hogar");
 			pc.setRowguid(14);
 			pc.setModifieddate(time);
 			
 			pc.setProductsubcategories(null);
 			
+			Productcategory temp = productcategoryRepository.save(pc);
+			assertNotNull(temp);
+			assertTrue(temp.getProductcategoryid() > 0);
+
+			Iterable<Productcategory> productcategory = productcategoryRepository.findAll();
+			Assertions.assertThat(productcategory).first().hasFieldOrPropertyWithValue("name", "Hogar");
 			
-		    
+			Productcategory pc1 = new Productcategory();
+			
+			pc1.setName("Todo terreno");
+			pc1.setRowguid(24);
+			pc1.setModifieddate(time);
+			
+			pc1.setProductsubcategories(null);
+			
+			Productcategory temp1 = productcategoryRepository.save(pc1);
+			assertNotNull(temp1);
+			assertEquals(2, temp1.getProductcategoryid());
+		}
+		
+	}
+	
+	@Nested
+	@DisplayName("Update methods")
+	class ProductcategoryServiceUpdate {
+		@Test
+		@Order(1)
+		public void update() {
+			//pc.setProductcategoryid(77);
+			pc.setName("Otra C");
+			pc.setRowguid(14);
+			pc.setModifieddate(time);
+			
+			pc.setProductsubcategories(null);
 			
 			Productcategory temp = productcategoryRepository.save(pc);
 			
-			Iterable<Productcategory> productcategory = productcategoryRepository.findAll();
-			Assertions.assertThat(productcategory).first().hasFieldOrPropertyWithValue("name", "Hogar");
-
-			
-			assertNotNull(temp);
-			
+			pc.setName("Otra C1");
+			Productcategory temp1 = productcategoryRepository.e(pc);
 		}
-	}
+	}*/
 
+	@Test
+	public void testFindById() {
+		Productcategory result = productcategoryRepository.findById(productcategory.getProductcategoryid()).get();
+		assertEquals(productcategory.getProductcategoryid(), result.getProductcategoryid());
+	}
+	
+	@Test
+	public void testFindAll() {
+		List<Productcategory> result = new ArrayList<>();
+		productcategoryRepository.findAll().forEach(e -> result.add(e));
+		assertEquals(result.size(), 1);
+	}
+	
+	@Test
+	public void testSave() {
+		Productcategory found = productcategoryRepository.findById(productcategory.getProductcategoryid()).get();
+		assertEquals(productcategory.getProductcategoryid(), found.getProductcategoryid());
+	}
 
 	@AfterAll
 	static void end() {
+		System.out.println("... TEST STARTED ...");
 	}
 
-	
 	@AfterEach
 	void tearDown() {
+		pc = null;
+		productcategoryRepository = null;
+		System.gc();
 	}
-
 
 }
